1bf927a41a6a7b6264fd46e27736197c21c61841
schimizzimj <marcusjschimizzi@gmail.com>
2023-09-25 16:44:31 -0700

    fix unstyled loading bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import ThemeProvider from './ThemeProvider';
 import Nav from './components/Nav';
+import StyledComponentsRegistry from './lib/registry';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,10 +17,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <body className={inter.className}>
-                <ThemeProvider>
-                    <Nav />
-                    {children}
-                </ThemeProvider>
+                <StyledComponentsRegistry>
+                    <ThemeProvider>
+                        <Nav />
+                        {children}
+                    </ThemeProvider>
+                </StyledComponentsRegistry>
             </body>
         </html>
     );
