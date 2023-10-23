2a540831e1ea1babef70bf4e85ae3c6b7bc26f2f
schimizzimj <marcusjschimizzi@gmail.com>
2023-10-22 22:52:44 -0700

    add git activity component

--- src/app/components/GitActivity/index.tsx
+++ src/app/components/GitActivity/index.tsx
@@ -1,35 +1,24 @@
 'use client';
-import { useQuery, gql } from '@apollo/client';
 import { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import { scaleSequential } from 'd3-scale';
-
-const GET_GIT_ACTIVITY = gql`
-    query GetGitActivity($username: String!) {
-        user(login: $username) {
-            contributionsCollection {
-                contributionCalendar {
-                    totalContributions
-                    weeks {
-                        contributionDays {
-                            contributionCount
-                            date
-                        }
-                    }
-                }
-            }
-        }
-    }
-`;
+import styled, { useTheme } from 'styled-components';
+import { ScaleLinear, scaleLinear } from 'd3-scale';
 
 interface ContributionDay {
     contributionCount: number;
     date: string;
+    color?: string;
 }
 
 interface Contributions {
     rows: ContributionDay[][];
 }
+
+const Wrapper = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 1.5rem 0;
+`;
 
 const Day = styled.td`
     width: 12px;
@@ -38,21 +27,23 @@ const Day = styled.td`
     border-radius: 2px;
     shape-rendering: geometricPrecision;
     background-color: #ebedf0;
+    transition: transform 0.2s ease-in-out;
+
+    &:hover {
+        transform: scale(1.25);
+    }
 `;
 
 const Row = styled.tr`
     height: 12px;
-    border-spacing: 4px;
     overflow: hidden;
     position: relative;
 `;
 
-const TableBody = styled.tbody`
-    border-spacing: 4px;
-`;
+const TableBody = styled.tbody``;
 
 const Table = styled.table`
-    border-spacing: 4px;
+    border-spacing: 5px;
     overflow: hidden;
     position: relative;
     width: max-content;
@@ -60,52 +51,46 @@ const Table = styled.table`
 `;
 
 const GitActivity = () => {
-    const [processedData, setProcessedData] = useState<Contributions | null>(null);
-    const [colorScale, setColorScale] = useState<string[]>([]);
-    const { loading, error, data } = useQuery(GET_GIT_ACTIVITY, {
-        variables: { username: 'schimizzimj' },
+    const colors = useTheme().colors;
+    const [data, setData] = useState<Contributions | null>(null);
+
+    useEffect(() => {
+        async function fetchGitActivity() {
+            const url = '/api/contributions';
+            const res = await fetch(url);
+            const data = await res.json();
+            setData(data);
+        }
+        if (!data) fetchGitActivity();
     });
 
     useEffect(() => {
         if (!data) return;
-        const transposedData = data.user.contributionsCollection.contributionCalendar.weeks.reduce(
-            (acc: any, week: any) => {
-                week.contributionDays.forEach((day: any, i: any) => {
-                    if (!acc[i]) acc[i] = [];
-                    acc[i].push(day);
-                });
-                return acc;
-            },
-            [],
-        );
-        const domain = transposedData.reduce((acc: any, week: any) => {
-            const weekTotal = week.reduce((acc: any, day: any) => acc + day.contributionCount, 0);
-            return [...acc, weekTotal];
-        }, []);
-        const scale = scaleSequential()
+        const domain = data.rows.map((week: any) => week.map((day: any) => day.contributionCount)).flat();
+        console.info({ domain, data });
+        const scale: ScaleLinear<any, unknown> = scaleLinear()
             .domain([Math.min(...domain), Math.max(...domain)])
-            .interpolator((t) => `rgb(0, 0, ${t * 255})`);
-        setColorScale(domain.map((d: any) => scale(d)));
-        setProcessedData({ rows: transposedData });
-    }, [data, setProcessedData]);
-
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :( {JSON.stringify(error, null, 2)}</p>;
-    if (data) console.log(data.user.contributionsCollection.contributionCalendar);
+            .range([colors.text1, colors.main1]);
+        for (const week of data.rows) {
+            for (const day of week) {
+                console.info({ color: scale(day.contributionCount) });
+                day.color = scale(day.contributionCount) as string;
+            }
+        }
+    }, [colors.main1, colors.text1, data]);
 
     return (
-        <div>
-            <h1>GitActivity</h1>
+        <Wrapper>
             <Table>
                 <TableBody>
-                    {processedData &&
-                        (processedData?.rows || []).map((week: any, i: any) => (
+                    {data &&
+                        (data?.rows || []).map((week: any, i: any) => (
                             <Row key={i}>
                                 {week.map((day: any, j: any) => (
                                     <Day
                                         key={j}
                                         style={{
-                                            backgroundColor: colorScale[day.contributionCount],
+                                            backgroundColor: day.color,
                                         }}
                                         title={`${day.contributionCount} contributions on ${day.date}`}
                                     ></Day>
@@ -114,7 +99,7 @@ const GitActivity = () => {
                         ))}
                 </TableBody>
             </Table>
-        </div>
+        </Wrapper>
     );
 };
 
