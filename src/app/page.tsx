abe5fef2dcfa75b42aa4d0e084d69719d9af49b7
schimizzimj <marcusjschimizzi@gmail.com>
2024-06-12 23:42:17 -0700

    update dep vulns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,7 @@ const DynamicProjects = dynamic(() => import('@/components/Projects'), { ssr: fa
 const DynamicExperience = dynamic(() => import('@/components/Experience'), { ssr: false });
 // const DynamicSkills = dynamic(() => import('./components/Skills'), { ssr: false });
 const DynamicContact = dynamic(() => import('@/components/Contact'), { ssr: false });
-// const DynamicGitActivity = dynamic(() => import('./components/GitActivity'), { ssr: false });
-// const DynamicRecentProjects = dynamic(() => import('./components/RecentProjects'), { ssr: false });
+const DynamicRecentProjects = dynamic(() => import('@/components/RecentProjects'), { ssr: false });
 const DynamicFooter = dynamic(() => import('@/components/Footer'), { ssr: false });
 
 export default function Home() {
@@ -23,8 +22,7 @@ export default function Home() {
             <DynamicExperience work={workExperience} education={education} />
             {/* <DynamicSkills /> */}
             <DynamicContact />
-            {/* <DynamicGitActivity /> */}
-            {/* <DynamicRecentProjects /> */}
+            <DynamicRecentProjects />
             <DynamicFooter />
         </>
     );
