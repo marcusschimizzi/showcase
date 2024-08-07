878c438c3bab0b8d48b871d8b2c2a943c74541c7
schimizzimj <marcusjschimizzi@gmail.com>
2024-08-06 20:36:40 -0700

    add quick guide to skills section

--- src/components/SkillsPage/index.tsx
+++ src/components/SkillsPage/index.tsx
@@ -16,7 +16,11 @@ const SkillsPage = () => {
     return (
         <StyledSkills className="py-40 text-center" id="skills">
             <StyledHeader className="text-5xl">Skills</StyledHeader>
-            <div className="mx-auto h-[50vh] max-w-6xl mt-24">
+            <h3 className="GrayText px-2">
+                Circle size indicates relative proficiency. Click on any skill to learn more.
+            </h3>
+
+            <div className="mx-auto h-[50vh] max-w-6xl mt-4 relative">
                 <Bubbles renderOnlyInViewport={true} />
             </div>
         </StyledSkills>
