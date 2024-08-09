c6be3b507b283024b999f1c544c8f5b0e1fdadc7
schimizzimj <marcusjschimizzi@gmail.com>
2024-08-09 15:16:42 -0700

    make styled and tailwind themes play nicer

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,6 +73,7 @@ export const colors = {
 };
 
 const config: Config = {
+    darkMode: 'class',
     content: [
         './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
