224c3c9e1e13d7a1902571397bfe180fb20b6129
schimizzimj <marcusjschimizzi@gmail.com>
2024-08-13 17:22:42 -0400

    fix wonky skills background

--- src/components/SkillsPage/index.tsx
+++ src/components/SkillsPage/index.tsx
@@ -7,9 +7,13 @@ const StyledHeader = styled.h1`
     color: ${({ theme }) => theme.colors.text1};
 `;
 
+const StyledSkills = styled.div`
+    background-color: ${({ theme }) => theme.colors.background2};
+`;
+
 const SkillsPage = () => {
     return (
-        <div className="py-40 text-center max-w-full overflow-x-hidden bg-white dark:bg-gray-900" id="skills">
+        <StyledSkills className="py-40 text-center max-w-full overflow-x-hidden" id="skills">
             <StyledHeader className="text-5xl">Skills</StyledHeader>
             <h3 className="GrayText px-2">
                 Circle size indicates relative proficiency. Click on any skill to learn more.
@@ -18,7 +22,7 @@ const SkillsPage = () => {
             <div className="mx-auto h-[80vh] md:h-[60vh] max-w-6xl mt-4 relative">
                 <Bubbles renderOnlyInViewport={true} />
             </div>
-        </div>
+        </StyledSkills>
     );
 };
 
