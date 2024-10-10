fcf0360e10785b07077f665b29ea00125de47407
Marcus Schimizzi <marcus@schimizzi.io>
2024-10-10 00:16:55 -0700

    feat(Contact): Add message for successful form submission

--- src/components/Contact/ContactForm/index.tsx
+++ src/components/Contact/ContactForm/index.tsx
@@ -13,6 +13,7 @@ const ContactForm: React.FC = () => {
         email: '',
         message: '',
     });
+    const [isDone, setIsDone] = useState(false);
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [submitStatus, setSubmitStatus] = useState<'success' | 'error' | null>(null);
 
@@ -41,6 +42,7 @@ const ContactForm: React.FC = () => {
             });
 
             if (response.ok) {
+                setIsDone(true);
                 setSubmitStatus('success');
                 setFormData({
                     name: '',
@@ -58,79 +60,90 @@ const ContactForm: React.FC = () => {
     };
 
     return (
-        <form
-            name="contact"
-            method="POST"
-            data-netlify="true"
-            data-netlify-honeypot="field-for-bots"
-            onSubmit={handleSubmit}
-            className="space-y-4"
-        >
-            <input type="hidden" name="form-name" value="contact" />
-            <div>
-                <label htmlFor="name" className="block text-sm font-medium" style={{ color: colors.text2 }}>
-                    Name
-                </label>
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    placeholder="What should I call you?"
-                    required
-                    value={formData.name}
-                    onChange={handleChange}
-                    className="mt-1 block p-2 text-lg w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                    style={{ backgroundColor: colors.background2, color: colors.text1 }}
-                />
-            </div>
-            <div>
-                <label htmlFor="email" className="block text-sm font-medium" style={{ color: colors.text2 }}>
-                    Email
-                </label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="Where should I send a reply?"
-                    required
-                    value={formData.email}
-                    onChange={handleChange}
-                    className="mt-1 block p-2 text-lg w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                    style={{ backgroundColor: colors.background2, color: colors.text1 }}
-                />
-            </div>
-            <div>
-                <label htmlFor="message" className="block text-sm font-medium" style={{ color: colors.text2 }}>
-                    Message
-                </label>
-                <textarea
-                    id="message"
-                    name="message"
-                    placeholder="What would you like to chat about?"
-                    required
-                    value={formData.message}
-                    onChange={handleChange}
-                    rows={4}
-                    className="mt-1 block p-2 text-lg w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                    style={{ backgroundColor: colors.background2, color: colors.text2 }}
-                />
-            </div>
-            <button
-                type="submit"
-                disabled={isSubmitting}
-                className="px-4 py-2 text-lg text-white rounded-md focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out overflow-hidden transform hover-brightness-110 hover:shadow-lg hover:-translate-y-1"
-                style={{
-                    background: `linear-gradient(135deg, ${colors.main1}, ${colors.main2})`,
-                    backgroundSize: '200% 200%',
-                    animation: 'gradientShift 5s ease infinite',
-                }}
-            >
-                {isSubmitting ? 'Sending...' : 'Send'}
-            </button>
-            {submitStatus === 'error' && (
-                <p className="text-red-600">Uh-oh! There was a problem submitting the form. Please try again.</p>
+        <>
+            {!isDone ? (
+                <form
+                    name="contact"
+                    method="POST"
+                    data-netlify="true"
+                    data-netlify-honeypot="field-for-bots"
+                    onSubmit={handleSubmit}
+                    className="space-y-4"
+                >
+                    <input type="hidden" name="form-name" value="contact" />
+                    <div>
+                        <label htmlFor="name" className="block text-sm font-medium" style={{ color: colors.text2 }}>
+                            Name
+                        </label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            placeholder="What should I call you?"
+                            required
+                            value={formData.name}
+                            onChange={handleChange}
+                            className="mt-1 block p-2 text-lg w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            style={{ backgroundColor: colors.background2, color: colors.text1 }}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium" style={{ color: colors.text2 }}>
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Where should I send a reply?"
+                            required
+                            value={formData.email}
+                            onChange={handleChange}
+                            className="mt-1 block p-2 text-lg w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            style={{ backgroundColor: colors.background2, color: colors.text1 }}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="message" className="block text-sm font-medium" style={{ color: colors.text2 }}>
+                            Message
+                        </label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            placeholder="What would you like to chat about?"
+                            required
+                            value={formData.message}
+                            onChange={handleChange}
+                            rows={4}
+                            className="mt-1 block p-2 text-lg w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            style={{ backgroundColor: colors.background2, color: colors.text2 }}
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        disabled={isSubmitting}
+                        className="px-4 py-2 text-lg text-white rounded-md focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out overflow-hidden transform hover-brightness-110 hover:shadow-lg hover:-translate-y-1"
+                        style={{
+                            background: `linear-gradient(135deg, ${colors.main1}, ${colors.main2})`,
+                            backgroundSize: '200% 200%',
+                            animation: 'gradientShift 5s ease infinite',
+                        }}
+                    >
+                        {isSubmitting ? 'Sending...' : 'Send'}
+                    </button>
+                    {submitStatus === 'error' && (
+                        <p className="text-red-600">
+                            Uh-oh! There was a problem submitting the form. Please try again.
+                        </p>
+                    )}
+                </form>
+            ) : (
+                <>
+                    <h3>Thank you for reaching out!</h3>
+                    <p>I&#39;ll follow up to the email you left.</p>
+                </>
             )}
-        </form>
+        </>
     );
 };
 
